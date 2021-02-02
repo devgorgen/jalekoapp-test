1a4c2e738576b65a258623a08788b737c194d4c0
Augusto Gorgen <augusto.gorgen@partner.bmw.com.br>
2021-02-02 20:54:32 -0300

    adding first structure

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+/**
+ * Initial component
+ */
+export default function App() {
+    return (
+        <View>
+            <Text>Jaleko APP</Text>
+        </View>
+    );
+}
